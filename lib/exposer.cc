6f849128af40b5d3d796b4716beda8251b79e186
Jupp Müller <jupp0r@gmail.com>
2016-10-15 18:00:42 +0200

    Refactor serialization

--- lib/exposer.cc
+++ lib/exposer.cc
@@ -7,6 +7,7 @@
 #include <google/protobuf/io/coded_stream.h>
 #include <google/protobuf/io/zero_copy_stream_impl.h>
 #include <google/protobuf/util/json_util.h>
+#include <google/protobuf/util/message_differencer.h>
 
 #include "exposer.h"
 
@@ -27,29 +28,51 @@ MetricsHandler::MetricsHandler(
       numScrapes_(numScrapesFamily_->add({})) {}
 
 static std::string serializeToDelimitedProtobuf(
-    const std::vector<std::weak_ptr<Collectable>>& collectables) {
+    const std::vector<io::prometheus::client::MetricFamily>& metrics) {
   std::ostringstream ss;
-  for (auto&& wcollectable : collectables) {
-    auto collectable = wcollectable.lock();
-    if (!collectable) {
-      continue;
+  for (auto&& metric : metrics) {
+    {
+      google::protobuf::io::OstreamOutputStream rawOutput{&ss};
+      google::protobuf::io::CodedOutputStream output(&rawOutput);
+
+      const int size = metric.ByteSize();
+      output.WriteVarint32(size);
     }
 
-    for (auto&& metricFamily : collectable->collect()) {
-      {
-        google::protobuf::io::OstreamOutputStream rawOutput{&ss};
-        google::protobuf::io::CodedOutputStream output(&rawOutput);
-
-        const int size = metricFamily.ByteSize();
-        output.WriteVarint32(size);
-      }
-
-      auto buffer = std::string{};
-      metricFamily.SerializeToString(&buffer);
-      ss << buffer;
-    }
+    auto buffer = std::string{};
+    metric.SerializeToString(&buffer);
+    ss << buffer;
   }
   return ss.str();
+}
+
+static std::string serializeToJson(
+    const std::vector<io::prometheus::client::MetricFamily>& metrics) {
+  using google::protobuf::util::MessageDifferencer;
+
+  std::stringstream ss;
+  ss << "[";
+
+  for (auto&& metric : metrics) {
+    std::string result;
+    google::protobuf::util::MessageToJsonString(
+        metric, &result, google::protobuf::util::JsonPrintOptions());
+    ss << result;
+    if (!MessageDifferencer::Equals(metric, metrics.back())) {
+      ss << ",";
+    }
+  }
+  ss << "]";
+  return ss.str();
+}
+
+static std::string serializeToHumanReadable(
+    const std::vector<io::prometheus::client::MetricFamily>& metrics) {
+  auto result = std::string{};
+  for (auto&& metric : metrics) {
+    result += metric.DebugString() + "\n";
+  }
+  return result;
 }
 
 static std::string getAcceptedEncoding(struct mg_connection* conn) {
@@ -68,68 +91,34 @@ bool MetricsHandler::handleGet(CivetServer* server,
   using namespace io::prometheus::client;
 
   auto acceptedEncoding = getAcceptedEncoding(conn);
+  auto metrics = collectMetrics();
+
+  auto body = std::string{};
+  auto contentType = std::string{};
+
   if (acceptedEncoding.find("application/vnd.google.protobuf") !=
       std::string::npos) {
-    auto body = serializeToDelimitedProtobuf(collectables_);
-    mg_printf(conn,
-              "HTTP/1.1 200 OK\r\n"
-              "Content-Type: "
-              "application/vnd.google.protobuf; "
-              "proto=io.prometheus.client.MetricFamily; "
-              "encoding=delimited\r\n"
-              "Content-Length: ");
-    mg_printf(conn, "%lu\r\n\r\n", body.size());
-    mg_write(conn, body.data(), body.size());
-    bytesTransfered_->inc(body.size());
+    body = serializeToDelimitedProtobuf(metrics);
+    contentType =
+        "application/vnd.google.protobuf; "
+        "proto=io.prometheus.client.MetricFamily; "
+        "encoding=delimited";
   } else if (acceptedEncoding.find("application/json") != std::string::npos) {
-    std::stringstream ss;
-    ss << "[";
-
-    for (auto&& wcollectable : collectables_) {
-      auto collectable = wcollectable.lock();
-      if (!collectable) {
-        continue;
-      }
-
-      for (auto&& metricFamily : collectable->collect()) {
-        std::string result;
-        google::protobuf::util::MessageToJsonString(
-            metricFamily, &result, google::protobuf::util::JsonPrintOptions());
-        ss << result;
-        if (collectable != collectables_.back().lock()) {
-          ss << ",";
-        }
-      }
-    }
-    ss << "]";
-    auto body = ss.str();
-    mg_printf(conn,
-              "HTTP/1.1 200 OK\r\n"
-              "Content-Type: application/json\r\n"
-              "Content-Length: ");
-    mg_printf(conn, "%lu\r\n\r\n", body.size());
-    mg_write(conn, body.data(), body.size());
-    bytesTransfered_->inc(body.size());
+    body = serializeToJson(metrics);
+    contentType = "application/json";
   } else {
-    auto body = std::string{};
-    for (auto&& wcollectable : collectables_) {
-      auto collectable = wcollectable.lock();
-      if (!collectable) {
-        continue;
-      }
-
-      for (auto&& metricFamily : collectable->collect()) {
-        body += metricFamily.DebugString() + "\n";
-      }
-      mg_printf(conn,
-                "HTTP/1.1 200 OK\r\n"
-                "Content-Type: text/plain\r\n"
-                "Content-Length: ");
-      mg_printf(conn, "%lu\r\n\r\n", body.size());
-      mg_write(conn, body.data(), body.size());
-      bytesTransfered_->inc(body.size());
-    }
+    body = serializeToHumanReadable(metrics);
+    contentType = "text/plain";
   }
+
+  mg_printf(conn,
+            "HTTP/1.1 200 OK\r\n"
+            "Content-Type: %s\r\n",
+            contentType.c_str());
+  mg_printf(conn, "Content-Length: %lu\r\n\r\n", body.size());
+  mg_write(conn, body.data(), body.size());
+
+  bytesTransfered_->inc(body.size());
 
   numScrapes_->inc();
   return true;
@@ -147,5 +136,23 @@ Exposer::Exposer(std::uint16_t port)
 void Exposer::registerCollectable(
     const std::weak_ptr<Collectable>& collectable) {
   collectables_.push_back(collectable);
+}
+
+std::vector<io::prometheus::client::MetricFamily>
+MetricsHandler::collectMetrics() const {
+  auto collectedMetrics = std::vector<io::prometheus::client::MetricFamily>{};
+
+  for (auto&& wcollectable : collectables_) {
+    auto collectable = wcollectable.lock();
+    if (!collectable) {
+      continue;
+    }
+
+    for (auto metric : collectable->collect()) {
+      collectedMetrics.push_back(metric);
+    }
+  }
+
+  return collectedMetrics;
 }
 }

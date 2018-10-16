5e1dfff6a5c9918f2f58236f76b53df382c10b24
Jerry Crunchtime <jerry.c.t@web.de>
2018-10-16 13:57:49 +0200

    Document counter and gauge

--- core/include/prometheus/counter.h
+++ core/include/prometheus/counter.h
@@ -5,12 +5,30 @@
 #include "prometheus/metric_type.h"
 
 namespace prometheus {
+
+/// \brief A counter metric to represent a monotonically increasing value.
+///
+/// This class represents the metric type counter:
+/// https://prometheus.io/docs/concepts/metric_types/#counter
+///
+/// The value of the counter can only increase. Example of counters are:
+/// - the number of requests served
+/// - tasks completed
+/// - errors
+///
+/// Do not use a counter to expose a value that can decrease - instead use a
+/// Gauge.
 class Counter {
  public:
   static const MetricType metric_type = MetricType::Counter;
 
+  /// \brief Increment the counter by 1.
   void Increment();
+
+  /// \brief Increment the counter by a given amount.
   void Increment(double);
+
+  /// \brief Get the current value of the counter.
   double Value() const;
 
   ClientMetric Collect();
@@ -18,4 +36,5 @@ class Counter {
  private:
   Gauge gauge_;
 };
+
 }  // namespace prometheus
--- core/include/prometheus/gauge.h
+++ core/include/prometheus/gauge.h
@@ -7,18 +7,44 @@
 
 namespace prometheus {
 
+/// \brief A gauge metric to represent a value that can arbitrarily go up and
+/// down.
+///
+/// The class represents the metric type gauge:
+/// https://prometheus.io/docs/concepts/metric_types/#gauge
+///
+/// Gauges are typically used for measured values like temperatures or current
+/// memory usage, but also "counts" that can go up and down, like the number of
+/// running processes.
 class Gauge {
  public:
   static const MetricType metric_type = MetricType::Gauge;
 
+  /// \brief Create a gauge that starts at 0.
   Gauge();
+
+  /// \brief Create a gauge that starts at the given amount.
   Gauge(double);
+
+  /// \brief Increment the gauge by 1.
   void Increment();
+
+  /// \brief Increment the gauge by the given amount.
   void Increment(double);
+
+  /// \brief Decrement the gauge by 1.
   void Decrement();
+
+  /// \brief Decrement the gauge by the given amount.
   void Decrement(double);
+
+  /// \brief Set the gauge to the given value.
   void Set(double);
+
+  /// \brief Set the gauge to the current unixtime in seconds.
   void SetToCurrentTime();
+
+  /// \brief Get the current value of the gauge.
   double Value() const;
 
   ClientMetric Collect();

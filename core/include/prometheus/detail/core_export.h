b6a1b4c2fc21931012ed035ecec36bd967345123
Gregor Jasny <gjasny@googlemail.com>
2019-08-31 19:35:03 +0200

    Implement symbol visibility build system agnostic
    Issue: #223

---
+++ core/include/prometheus/detail/core_export.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#if defined(_WIN32)
+#  ifdef PROMETHEUS_CPP_COMPILE_CORE
+#    define PROMETHEUS_CPP_CORE_EXPORT __declspec(dllexport)
+#  else
+#    define PROMETHEUS_CPP_CORE_EXPORT __declspec(dllimport)
+#  endif
+#else
+#  define PROMETHEUS_CPP_CORE_EXPORT __attribute__((visibility("default")))
+#endif

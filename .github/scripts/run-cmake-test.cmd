083dae10b172acb75b18521551014d5e37b54b86
Gregor Jasny <gjasny@googlemail.com>
2020-03-16 17:00:36 +0100

    ci: use vcpkg for external dependencies

--- .github/scripts/run-cmake-test.cmd
+++ .github/scripts/run-cmake-test.cmd
@@ -5,3 +5,13 @@ cmake --build . --config Debug || EXIT /B 1
 cmake --build . --config Release || EXIT /B 1
 ctest -C Debug -V -LE Benchmark || EXIT /B 1
 ctest -C Release -V || EXIT /B 1
+cd .. || EXIT /B 1
+
+mkdir "_build" || EXIT /B 1
+cd "_build" || EXIT /B 1
+cmake .. -DUSE_THIRDPARTY_LIBRARIES=OFF -DCMAKE_TOOLCHAIN_FILE=%VCPKG_INSTALLATION_ROOT%\scripts\buildsystems\vcpkg.cmake || EXIT /B 1
+cmake --build . --config Debug || EXIT /B 1
+cmake --build . --config Release || EXIT /B 1
+ctest -C Debug -V -LE Benchmark || EXIT /B 1
+ctest -C Release -V || EXIT /B 1
+cd .. || EXIT /B 1
--- .github/scripts/run-prepare.cmd
+++ .github/scripts/run-prepare.cmd
@@ -1,3 +1,5 @@
 if [%1] == [bazel] (
     choco install bazel -y || EXIT /B 1
 )
+
+%VCPKG_INSTALLATION_ROOT%/vcpkg install benchmark civetweb curl gtest zlib

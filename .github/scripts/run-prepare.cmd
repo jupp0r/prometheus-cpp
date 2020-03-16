09d36a5037278ce392fe88b3809e237b238e2901
Gregor Jasny <gjasny@googlemail.com>
2020-03-16 17:00:36 +0100

    ci: use vcpkg for external dependencies

--- .github/scripts/run-prepare.cmd
+++ .github/scripts/run-prepare.cmd
@@ -1,3 +1,8 @@
 if [%1] == [bazel] (
     choco install bazel -y || EXIT /B 1
 )
+
+if [%1] == [cmake] (
+    %VCPKG_INSTALLATION_ROOT%/vcpkg install benchmark civetweb curl gtest zlib || EXIT /B 1
+)
+

81a3da1cc0fbe220a46cc57329f1242e7fe36bc6
Mark Klein (mklein201) <mark_klein@comcast.com>
2020-11-17 11:50:02 -0800

    Remove unused mocks, refactor mg_ mock

--- pull/tests/unit/handler_test.cc
+++ pull/tests/unit/handler_test.cc
@@ -6,40 +6,35 @@
 
 #include <gmock/gmock.h>
 
-
-/*
- * Dummies to resolve some callbacks that couldn't be mocked
- */
 #ifdef MOCK_MG
-CIVETWEB_API const char *mg_get_header(const struct mg_connection *,
-                                       const char *name) {return nullptr;}
-CIVETWEB_API int mg_printf(struct mg_connection *,
-                           PRINTF_FORMAT_STRING(const char *fmt),
-                           ...) {return 0;}
-CIVETWEB_API int mg_write(struct mg_connection *, const void *buf, size_t len) {return 0;}
-#endif
-
 /*
- * Mock the stuff not being used
+ * These symbols are hidden with cmake, but not bazel. Just dummy them up for the test.
  */
-class MetricsHandlerTest : public prometheus::detail::MetricsHandler {
-  public:
-    MetricsHandlerTest(prometheus::Registry& registry) : prometheus::detail::MetricsHandler(registry) {}
-    ~MetricsHandlerTest() {}
-
-    MOCK_METHOD(bool, handleDelete, (CivetServer* server, struct mg_connection* conn), (override));
-    MOCK_METHOD(bool, handleGet,    (CivetServer* server, struct mg_connection* conn), (override));
-    MOCK_METHOD(bool, handleHead,   (CivetServer* server, struct mg_connection* conn), (override));
-    MOCK_METHOD(bool, handleOptions,(CivetServer* server, struct mg_connection* conn), (override));
-    MOCK_METHOD(bool, handlePatch,  (CivetServer* server, struct mg_connection* conn), (override));
-    MOCK_METHOD(bool, handlePost,   (CivetServer* server, struct mg_connection* conn), (override));
-    MOCK_METHOD(bool, handlePut,    (CivetServer* server, struct mg_connection* conn), (override));
-};
+const char * mg_get_header(const struct mg_connection *conn, const char *name)
+{
+  (void) conn;
+  (void) name;
+  return nullptr;
+}
+int mg_printf(struct mg_connection *conn, const char *fmt, ...)
+{
+  (void) conn;
+  (void) fmt;
+  return 0;
+}
+int mg_write(struct mg_connection *conn, const void *buf, size_t len)
+{
+  (void) conn;
+  (void) buf;
+  (void) len;
+  return 0;
+}
+#endif
 
 TEST(HandlerTest, removeCollectable) {
 
   std::shared_ptr<prometheus::Registry>  registry(std::make_shared<prometheus::Registry>());;
-  MetricsHandlerTest handler(*registry);
+  prometheus::detail::MetricsHandler handler(*registry);
 
   std::shared_ptr<prometheus::Registry>  collectable(std::make_shared<prometheus::Registry>());
 
@@ -56,7 +51,7 @@ TEST(HandlerTest, removeCollectable) {
 
   metrics = handler.getTestMetrics();
 
-  ASSERT_EQ(1, metrics.size());
+  ASSERT_EQ(1u, metrics.size());
   ASSERT_EQ("time_running_seconds_total", metrics[0].name);
   ASSERT_EQ("How many seconds is this server running?", metrics[0].help);
   ASSERT_EQ(prometheus::MetricType::Counter, metrics[0].type);
@@ -64,5 +59,5 @@ TEST(HandlerTest, removeCollectable) {
   handler.RemoveCollectable(collectable);
 
   metrics = handler.getTestMetrics();
-  ASSERT_EQ(0, metrics.size());
+  ASSERT_EQ(0u, metrics.size());
 }

bb017ec15a824d3301845a1274b4b46a01d6d871
Gregor Jasny <gjasny@googlemail.com>
2020-11-23 23:44:04 +0100

    pull: Use internal base64 decoding function to avoid dependency (#417)

--- bazel/cppcodec.BUILD
+++
@@ -1,6 +0,0 @@
-cc_library(
-    name = "cppcodec",
-    hdrs = glob(["cppcodec/**/*.hpp"]),
-    includes = ["."],
-    visibility = ["//visibility:public"],
-)
--- cmake/Findcppcodec.cmake
+++
@@ -1,8 +0,0 @@
-find_path(CPPCODEC_INCLUDE_DIR base64_rfc4648.hpp PATH_SUFFIXES include/cppcodec)
-
-include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(cppcodec DEFAULT_MSG CPPCODEC_INCLUDE_DIR)
-
-if(cppcodec_FOUND)
-  set(CPPCODEC_INCLUDE_DIRS "${CPPCODEC_INCLUDE_DIR}")
-endif()

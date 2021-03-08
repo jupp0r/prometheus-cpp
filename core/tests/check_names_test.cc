1d36d42210bc57cfc0b4ccdc87ff2cee44669de7
Gregor Jasny <gjasny@googlemail.com>
2021-03-08 20:29:25 +0100

    core: Replace name check regex (#462)
    * core: test for more invalid lables
    
    * core: rewrite label and metric name checks
    
    The std::regex has the problem that it is not available everywhere
    and also it's 75 times slower than the naive validity check.
    
    ```
    BM_Label_Check_Regex          550 ns          550 ns      1270601
    BM_Label_Check_NoRegex       7.50 ns         7.50 ns     92802503
    ```

--- core/tests/check_names_test.cc
+++ core/tests/check_names_test.cc
@@ -16,6 +16,15 @@ TEST(CheckNamesTest, malformed_metric_name) {
   EXPECT_FALSE(CheckMetricName("fa mi ly with space in name or |"));
 }
 TEST(CheckNamesTest, empty_label_name) { EXPECT_FALSE(CheckLabelName("")); }
+TEST(CheckNamesTest, invalid_label_name) {
+  EXPECT_FALSE(CheckLabelName("log-level"));
+}
+TEST(CheckNamesTest, leading_invalid_label_name) {
+  EXPECT_FALSE(CheckLabelName("-abcd"));
+}
+TEST(CheckNamesTest, trailing_invalid_label_name) {
+  EXPECT_FALSE(CheckLabelName("abcd-"));
+}
 TEST(CheckNamesTest, good_label_name) { EXPECT_TRUE(CheckLabelName("type")); }
 TEST(CheckNamesTest, reserved_label_name) {
   EXPECT_FALSE(CheckMetricName("__some_reserved_label"));

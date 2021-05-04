6139f9af92ae787c0cd5f42323cd8ea1a8d2c8bf
Gustavo de Souza <gustavo.alexandre@gmail.com>
2021-05-04 13:37:04 -0400

    Create make_deb.sh

---
+++ make_deb.sh
@@ -0,0 +1,43 @@
+#/bin/bash -ex
+# Create debian package
+
+echo "Usage: $0 <version>"
+
+if [ -z "$1" ]; then
+    echo "You must provide a version name";
+    exit -1;
+fi
+
+# Configure your paths and filenames
+DEBPACKAGE="prometheus-cpp"
+DEBVERSION=$(grep project CMakeLists.txt | grep VERSION | awk -F ' ' '{print $3}' | awk -F ')' '{print $1}')
+DEBAVIDBOTSVERSION="$1"
+DEBARCH="amd64"
+PACKAGE=${DEBPACKAGE}_${DEBVERSION}-avidbots${DEBAVIDBOTSVERSION}_${DEBARCH}
+
+# Create package folder structure
+rm -rf build
+mkdir -p build/$PACKAGE
+
+# build
+pushd build
+#cmake .. -DINSTALL_DIR=$PACKAGE/opt/$DEBPACKAGE
+cmake .. -DBUILD_SHARED_LIBS=ON
+make 
+mkdir -p $PACKAGE
+make DESTDIR=`pwd`/$PACKAGE install
+
+# Create debian package control file based on the package
+pushd $PACKAGE
+mkdir -p DEBIAN
+cat << EOF > DEBIAN/control
+Package: $DEBPACKAGE
+Architecture: all
+Version: $DEBVERSION
+Maintainer: Frank Imerson <frank.imerson@avidbots.com>
+Homepage: https://github.com/avidbots/prometheus-cpp
+Description: This library aims to enable Metrics-Driven Development for C++ services
+EOF
+popd 
+
+dpkg-deb --build $PACKAGE

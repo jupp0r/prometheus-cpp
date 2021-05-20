8d57549e8a7f0f1b9a67afc572777503991f158a
Gustavo de Souza <gustavo.alexandre@gmail.com>
2021-05-20 10:22:31 -0400

    Add deb revision to control file

--- make_deb.sh
+++ make_deb.sh
@@ -12,8 +12,9 @@ fi
 DEBPACKAGE="prometheus-cpp"
 DEBVERSION=$(grep project CMakeLists.txt | grep VERSION | awk -F ' ' '{print $3}' | awk -F ')' '{print $1}')
 DEBAVIDBOTSVERSION="$1"
+DEBREVISION=avidbots${DEBAVIDBOTSVERSION}
 DEBARCH="amd64"
-PACKAGE=${DEBPACKAGE}_${DEBVERSION}-avidbots${DEBAVIDBOTSVERSION}_${DEBARCH}
+PACKAGE=${DEBPACKAGE}_${DEBVERSION}-${DEBREVISION}_${DEBARCH}
 
 # Create package folder structure
 rm -rf build
@@ -34,7 +35,7 @@ mkdir -p DEBIAN
 cat << EOF > DEBIAN/control
 Package: $DEBPACKAGE
 Architecture: all
-Version: $DEBVERSION
+Version: $DEBVERSION-$DEVREVISION
 Maintainer: Frank Imerson <frank.imerson@avidbots.com>
 Homepage: https://github.com/avidbots/prometheus-cpp
 Description: This library aims to enable Metrics-Driven Development for C++ services

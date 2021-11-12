7075a2b657a8c101100c43ba89505fbe7394bb90
Gregor Jasny <gjasny@googlemail.com>
2021-11-12 19:02:22 +0100

    fix(core): gracefully handle label hash collisions
    ```
    before:
    
    BM_Registry_CreateFamily             49.2 ns         49.0 ns     11087352
    BM_Registry_CreateCounter/0           165 ns          165 ns      4170191
    BM_Registry_CreateCounter/1          1967 ns         1958 ns       556346
    BM_Registry_CreateCounter/8          4280 ns         4269 ns       176711
    BM_Registry_CreateCounter/64        27091 ns        27009 ns        25755
    BM_Registry_CreateCounter/512      224201 ns       223581 ns         3023
    BM_Registry_CreateCounter/4096    2056280 ns      2049876 ns          330
    
    after:
    
    BM_Registry_CreateFamily             47.8 ns         47.8 ns     15131733
    BM_Registry_CreateCounter/0           158 ns          158 ns      4295006
    BM_Registry_CreateCounter/1          1009 ns         1009 ns      1169219
    BM_Registry_CreateCounter/8          3238 ns         3231 ns       203179
    BM_Registry_CreateCounter/64        25989 ns        25949 ns        27008
    BM_Registry_CreateCounter/512      211288 ns       211220 ns         3265
    BM_Registry_CreateCounter/4096    1960240 ns      1959743 ns          350
    ```
    
    Fixes: #532

--- core/tests/utils_test.cc
+++ core/tests/utils_test.cc
@@ -9,26 +9,32 @@ namespace prometheus {
 
 namespace {
 
-TEST(UtilsTest, hash_labels_1) {
+class UtilsTest : public testing::Test {
+ public:
+  detail::LabelHasher hasher;
+};
+
+TEST_F(UtilsTest, hash_labels_1) {
   std::map<std::string, std::string> labels;
   labels.insert(std::make_pair<std::string, std::string>("key1", "value1"));
   labels.insert(std::make_pair<std::string, std::string>("key2", "vaule2"));
-  auto value1 = detail::hash_labels(labels);
-  auto value2 = detail::hash_labels(labels);
+
+  auto value1 = hasher(labels);
+  auto value2 = hasher(labels);
 
   EXPECT_EQ(value1, value2);
 }
 
-TEST(UtilsTest, hash_labels_2) {
+TEST_F(UtilsTest, hash_labels_2) {
   std::map<std::string, std::string> labels1{{"aa", "bb"}};
   std::map<std::string, std::string> labels2{{"a", "abb"}};
-  EXPECT_NE(detail::hash_labels(labels1), detail::hash_labels(labels2));
+  EXPECT_NE(hasher(labels1), hasher(labels2));
 }
 
-TEST(UtilsTest, hash_label_3) {
+TEST_F(UtilsTest, hash_label_3) {
   std::map<std::string, std::string> labels1{{"a", "a"}};
   std::map<std::string, std::string> labels2{{"aa", ""}};
-  EXPECT_NE(detail::hash_labels(labels1), detail::hash_labels(labels2));
+  EXPECT_NE(hasher(labels1), hasher(labels2));
 }
 
 }  // namespace

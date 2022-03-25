1707ea40fca61d716f3c0b4d6d15a607502575d8
Leith Bade <leith@swiftnav.com>
2022-03-25 23:09:52 +1100

    Set clang-format to always left align references and pointers

--- core/src/serializer.cc
+++ core/src/serializer.cc
@@ -5,7 +5,7 @@
 namespace prometheus {
 
 std::string Serializer::Serialize(
-    const std::vector<MetricFamily> &metrics) const {
+    const std::vector<MetricFamily>& metrics) const {
   std::ostringstream ss;
   Serialize(ss, metrics);
   return ss.str();

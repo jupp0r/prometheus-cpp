1bf2452c8fb700afa577538358ad292ab9c40e9b
Gregor Jasny <gjasny@googlemail.com>
2022-12-20 14:25:54 +0100

    wip

--- conanfile.py
+++ conanfile.py
@@ -4,17 +4,20 @@ from conan import ConanFile
 from conan.tools.cmake import CMakeToolchain
 from conan.tools.cmake import cmake_layout
 
+required_conan_version = ">=1.55.0"
 class PrometheusCpp(ConanFile):
     settings = "os", "arch", "compiler", "build_type"
     generators = "CMakeDeps"
 
     def build_requirements(self):
-        #if self.settings.os != "Windows":
-        #    self.build_requires("benchmark/1.7.1")
-        #self.build_requires("civetweb/1.15")        
-        #self.build_requires("libcurl/7.86.0")        
-        self.build_requires("gtest/1.12.1")
-        self.build_requires("zlib/1.2.13")        
+        if self.settings.os != "Windows":
+           self.test_requires("benchmark/1.7.1")
+        self.test_requires("gtest/1.12.1")
+
+    def requirements(self):
+        self.requires("civetweb/1.15")
+        self.requires("libcurl/7.86.0")
+        self.requires("zlib/1.2.13")
 
     def configure(self):
         self.options['civetweb'].with_ssl = False
@@ -22,8 +25,8 @@ class PrometheusCpp(ConanFile):
     def generate(self):
         tc = CMakeToolchain(self)
         tc.cache_variables["USE_THIRDPARTY_LIBRARIES"] = False
-        tc.cache_variables["ENABLE_PULL"] = False
-        tc.cache_variables["ENABLE_PUSH"] = False
+        #tc.cache_variables["ENABLE_PULL"] = False
+        #tc.cache_variables["ENABLE_PUSH"] = False
         #tc.variables["MYVAR"] = "MYVAR_VALUE"
         #tc.preprocessor_definitions["MYDEFINE"] = "MYDEF_VALUE"
         #tc.user_presets_path = os.path.join(self.source_folder, '..')

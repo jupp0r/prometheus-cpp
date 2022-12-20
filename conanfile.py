76549ffb10d5b0c9dbee4b3a78646076c02f3ba6
Gregor Jasny <gjasny@googlemail.com>
2022-12-20 11:23:10 +0100

    wip

--- conanfile.py
+++ conanfile.py
@@ -13,7 +13,7 @@ class PrometheusCpp(ConanFile):
         #    self.build_requires("benchmark/1.7.1")
         #self.build_requires("civetweb/1.15")        
         #self.build_requires("libcurl/7.86.0")        
-        #self.build_requires("gtest/1.12.1")        
+        self.build_requires("gtest/1.12.1")
         self.build_requires("zlib/1.2.13")        
 
     def configure(self):
@@ -22,6 +22,8 @@ class PrometheusCpp(ConanFile):
     def generate(self):
         tc = CMakeToolchain(self)
         tc.cache_variables["USE_THIRDPARTY_LIBRARIES"] = False
+        tc.cache_variables["ENABLE_PULL"] = False
+        tc.cache_variables["ENABLE_PUSH"] = False
         #tc.variables["MYVAR"] = "MYVAR_VALUE"
         #tc.preprocessor_definitions["MYDEFINE"] = "MYDEF_VALUE"
         #tc.user_presets_path = os.path.join(self.source_folder, '..')

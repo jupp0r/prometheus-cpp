37a24e40568647ab86f65c64a4f983708c0a5a20
Gregor Jasny <gjasny@googlemail.com>
2022-12-20 10:24:23 +0100

    wup

--- conanfile.py
+++ conanfile.py
@@ -9,21 +9,17 @@ class PrometheusCpp(ConanFile):
     generators = "CMakeDeps"
 
     def build_requirements(self):
-        self.build_requires("benchmark/1.7.1")
+        if self.settings.os != "Windows":
+            self.build_requires("benchmark/1.7.1")
         self.build_requires("civetweb/1.15")        
         self.build_requires("libcurl/7.86.0")        
         self.build_requires("gtest/1.12.1")        
         self.build_requires("zlib/1.2.13")        
 
+    def configure(self):
+        self.options['civetweb'].with_ssl = False
+
     def generate(self):
-        #self.folders.build_folder = self.generators_folder
-        print(vars(self.layouts.build))
-        print(self.package_folder)
-        print(self.export_folder)
-        print(self.source_folder)
-        print(self.generators_folder)
-        print(self.build_folder)
-        print('----')
         tc = CMakeToolchain(self)
         tc.cache_variables["USE_THIRDPARTY_LIBRARIES"] = False
         #tc.variables["MYVAR"] = "MYVAR_VALUE"

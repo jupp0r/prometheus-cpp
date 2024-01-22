2f528e2bdc203b35b734321f675edb4731285ee4
Gregor Jasny <gjasny@googlemail.com>
2024-01-22 07:46:57 +0100

    test(pull): fix build with shared libs
    Fix: #687

--- push/src/detail/label_encoder.h
+++ push/src/detail/label_encoder.h
@@ -3,11 +3,12 @@
 #include <iosfwd>
 
 #include "prometheus/labels.h"
+#include "prometheus/detail/push_export.h"
 
 namespace prometheus {
 namespace detail {
 
-void encodeLabel(std::ostream& os, const Label& label);
+PROMETHEUS_CPP_PUSH_EXPORT void encodeLabel(std::ostream& os, const Label& label);
 
 }
 }  // namespace prometheus
